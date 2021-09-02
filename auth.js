f1f8e3bef8274a1aa08ab3566ca6ca44a3122ceb
drjlevi6 <drjlevi6@aol.com>
2021-09-02 11:20:20 -0400

    Update auth.js
    Inserted console.log

--- auth.js
+++ auth.js
@@ -16,6 +16,7 @@ let generateJWTToken = (user) => {
 /* POST login: */
 module.exports = (router) => {
 	router.post('/login', (req, res) => {
+		console.log(req);
 		passport.authenticate('local', { session: false }, 
 				(error, user, info) => {
 			console.log(error);
